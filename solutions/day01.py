e53411e391b259f5224e9f75b2400a3a9588b61f
Timothy Andrew <mail@timothyandrew.net>
2026-01-13 01:01:39 +0000

    day 1 part 1

--- solutions/day01.py
+++ solutions/day01.py
@@ -2,12 +2,26 @@
 
 from utils import read_input, read_lines
 
+def parse_line(line):
+    if line[0] == 'R':
+        return int(line[1:])
+    else:
+        return -1 * int(line[1:])
 
 def part1():
     """Solve part 1."""
-    lines = read_lines(1, "sample")
-    pass
+    lines = [parse_line(line) for line in read_lines(1, "puzzle")]
 
+    position = 50
+    result = 0
+
+    for line in lines:
+        position = (position + line) % 100
+
+        if position == 0:
+            result += 1
+
+    return result
 
 def part2():
     """Solve part 2."""
